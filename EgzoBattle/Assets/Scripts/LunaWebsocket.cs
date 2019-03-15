ab6b8b7e96b62056934e0857f790055d475bdeb6
Lucas <35385484+V-o-y-a-g-e-r@users.noreply.github.com>
2019-03-15 22:01:15 +0100

    Added Luna socket.

---
+++ EgzoBattle/Assets/Scripts/LunaWebsocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fleck;
+using System;
+
+public class LunaWebsocket : MonoBehaviour
+{
+
+    protected LunaWebsocket() { }
+   
+    private void Awake() {
+
+        WebSocketServer server = new WebSocketServer("ws://192.168.102.59:1235");
+
+        server.Start(socket =>
+        {  
+            socket.OnOpen = () => Debug.Log("Open!");
+            socket.OnClose = () => Debug.Log("Close!");
+            socket.OnMessage += RecieveJsonObject;
+        });
+    }
+
+    private void RecieveJsonObject(string obj)
+    {
+        Debug.Log(obj);
+        //Data xd = JsonUtility.FromJson<Data>(obj);
+        //Debug.Log(xd);
+    }
+
+    private class Data
+    {
+        public double value {get;set;}
+    }
+}
