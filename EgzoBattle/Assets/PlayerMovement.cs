8923e27659c07029c2562732fdf1b8793220a9d1
Lucas <35385484+V-o-y-a-g-e-r@users.noreply.github.com>
2019-03-15 20:22:01 +0100

    Added sideways movement.

--- EgzoBattle/Assets/PlayerMovement.cs
+++ EgzoBattle/Assets/PlayerMovement.cs
@@ -6,12 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody rigidbody;
+    public float sidewaysForce;
     void Start()
     {
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetKey("a")) {
+            rigidbody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d")) {
+            rigidbody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
     }
 }
