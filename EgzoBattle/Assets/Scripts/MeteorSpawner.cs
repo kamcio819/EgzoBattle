d70ad60c8e217eb27513b6cd3d161bec687160af
kamcio819 <kamcio819@gmail.com>
2019-03-16 03:56:07 +0100

    Pooling

--- EgzoBattle/Assets/Scripts/MeteorSpawner.cs
+++ EgzoBattle/Assets/Scripts/MeteorSpawner.cs
@@ -1,15 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MeteorSpawner : MonoBehaviour, IUpdateable
 {
+    [SerializeField]
+    private MeteorSpawnerController meteorSpawnerController;
 
     [SerializeField]
     private List<MeteorObject> meteorObjectCollection = new List<MeteorObject>();
 
+    private float timeStep;
+
+    private float timeConst = 10f;
+
    public void OnUpdate()
    {
-      
+       timeStep += Time.deltaTime;
+       if(timeStep > timeConst) {
+           SpawnMeteor();
+       }
+   }
+
+   private void SpawnMeteor()
+   {
+      throw new NotImplementedException();
+   }
+
+   public void Start()
+   {
+       for(int i = 0; i < meteorObjectCollection.Count; ++i) {
+        MyObjectPoolManager.Instance.CreatePoolIfNotExists(meteorObjectCollection[i].gameObject, 10, 25, false);
+       }
    }
 }
---
+++ EgzoBattle/Assets/Scripts/MeteorSpawnerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawnerController : MonoBehaviour
+{
+    private float timeStep;
+    private float timeConst = 10f;
+    public void SpawnMeteors() {
+        timeStep += Time.deltaTime;
+       if(timeStep > timeConst) {
+           SpawnMeteor();
+       }
+    }
+
+    
+   private void SpawnMeteor()
+   {
+      GameObject meteor = MyObjectPoolManager.Instance.GetObject("Meteor", true);
+   }
+
+}
