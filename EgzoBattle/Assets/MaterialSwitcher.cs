d2260a8a3e38bd785fb17d22052e908a0759f566
kamcio819 <kamcio819@gmail.com>
2019-06-02 13:11:38 +0200

    Fixed planet chooser

---
+++ EgzoBattle/Assets/MaterialSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwitcher : MonoBehaviour
+{
+    private void OnEnable() {
+        var mats = GetComponent<Renderer>().materials;
+        mats[0] = GameController.Instance.ChoosedMaterial;
+        GetComponent<Renderer>().materials = mats;
+    }
+}
---
+++ EgzoBattle/Assets/Scripts/GameController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : Singleton<GameController>
+{
+    [SerializeField]
+    private Material choosedMaterial;
+
+    public Material ChoosedMaterial { get => choosedMaterial; set => choosedMaterial = value; }
+}
