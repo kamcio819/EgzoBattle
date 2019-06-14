e10d0c94fa3663cc093d4e7b10136f021512fd2e
nefarinus <tatonmpr@gmail.com>
2019-06-14 22:15:00 +0200

    Almost working rotations

--- EgzoBattle/Assets/Scripts/ShipAnimationController.cs
+++ EgzoBattle/Assets/Scripts/ShipAnimationController.cs
@@ -28,29 +28,36 @@ public class ShipAnimationController : MonoBehaviour
     {
         onEndIdle();
         KillCurentlyWorkingTweens();
-        rotateLeft = spaceShip.DOLocalRotate(new Vector3(maxRotateLeft, 0, 0), 1f, RotateMode.Fast);
+        rotateLeft = spaceShip.DOLocalRotate(new Vector3(maxRotateLeft, 0, 0), GetDuration(maxRotateLeft), RotateMode.Fast);
 
     }
     public void StartRotateRight()
     {
         onEndIdle();
         KillCurentlyWorkingTweens();
-        rotateRight = spaceShip.DOLocalRotate(new Vector3(maxRotateRight, 0, 0), 1f, RotateMode.Fast);
+        rotateRight = spaceShip.DOLocalRotate(new Vector3(maxRotateRight, 0, 0), GetDuration(maxRotateRight), RotateMode.Fast);
 
     }
     public void StartRotateOriginal()
     {
 
         KillCurentlyWorkingTweens();
-        rotateToOriginal = spaceShip.DOLocalRotate(new Vector3(defaultRotation, 0, 0), 1f, RotateMode.Fast).OnComplete(() => { onStartIdle(); });
+        rotateToOriginal = spaceShip.DOLocalRotate(new Vector3(defaultRotation, 0, 0), GetDuration(defaultRotation), RotateMode.Fast).OnComplete(() => { onStartIdle(); });
     }
     public float GetDuration(float maxRotation)
     {
+
         float currentRotation = spaceShip.localRotation.eulerAngles.x;
-        float rotationToMaxRotation = maxRotation - currentRotation;
+        float rotationToMaxRotation = Mathf.Abs(maxRotation - currentRotation);
+
+        Debug.Log(rotationToMaxRotation);
+        if (rotationToMaxRotation > 180)
+        {
+            rotationToMaxRotation = 360 - rotationToMaxRotation;
+        }
+
         float duration = (rotationToMaxRotation / 180) * maxDuration;
-        Debug.Log(Mathf.Abs(rotationToMaxRotation));
-        return Mathf.Abs(duration);
+        return duration;
     }
 
     public void KillCurentlyWorkingTweens()
