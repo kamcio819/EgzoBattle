24614e68512639b6120c6e3f34719dbc2503d8f7
kamcio819 <kamcio819@gmail.com>
2019-06-17 00:00:47 +0200

    fixed menu

---
+++ EgzoBattle/Assets/UIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> menuScreens;
+
+    public void OpenScreen(string screenName) {
+        menuScreens.ForEach(x => x.SetActive(false));
+        var screenToOpen = menuScreens.Find(x => x.name == screenName);
+        screenToOpen.SetActive(true);
+    }
+}
